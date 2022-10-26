fc548398078aa2330681261c26545054de0a1ce3
Carlos-Castellanos <carloslopez@itmexicali.edu.mx>
2022-10-25 17:11:10 -0700

    filter by date range

--- static/js/multiple.js
+++ static/js/multiple.js
@@ -1,18 +1,16 @@
 const labels = ["2021-01-01", "2021-01-02", "2021-01-03", "2021-01-04", "2021-01-05", "2021-01-06", "2021-01-07", "2021-01-08", "2021-01-09", "2021-01-10",];
-
 console.log({ totaltemperatura })
 console.log(labels)
 
 let dates = totaldates;
 //let dates = Label_Seconds;
-
 let temp = totaltemperatura;
 let hum = totalhumedad;
 let pres = totalpresion;
 let pm25 = totalpm25;
 let pm10 = totalpm10;
-
 let ctx = document.getElementById("myChart").getContext('2d');
+
 let myChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -93,3 +91,47 @@ let myChart = new Chart(ctx, {
         },
     }
 });
+
+
+function filterDate() {
+    const start1 = new Date(document.getElementById('start').value);
+    const start = start1.setHours(0, 0, 0, 0)
+    const end1 = new Date(document.getElementById('end').value);
+    const end = end1.setHours(0, 0, 0, 0)
+    const ss = start + 144000000;
+    const filterDates = dates.filter(date => date >= (start + 144000000) && date <= (end + 144000000));
+    console.log(start)
+    console.log(end)
+    console.log(filterDates)
+    myChart.config.data.labels = filterDates;
+    myChart.update();
+};
+
+function resetDate() {
+    myChart.config.data.labels = dates;
+    myChart.update();
+}
+
+function init() {
+    //hook events, triggeres events
+    console.log("init");
+    document.getElementById("btnFilter").addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log("btnFilter");
+        filterDate();
+
+    });
+    document.getElementById("btnReset").addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log("btnReset");
+        resetDate();
+
+    });
+
+}
+
+window.onpageshow = function () {
+    console.log("algo");
+};
+window.onload = init;
+
